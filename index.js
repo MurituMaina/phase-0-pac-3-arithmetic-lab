039d18267980d62c9ac066faa59766d25c4d1220
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-03 10:31:56 +0300

    Update Progress

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+
+    return a + b;
+}
+
+add(12, 13);
+
+function subtract(b, a) {
+
+    return b - a;
+}
+subtract(44, 33);
+
+function multiply(a, b) {
+    return a * b;
+}
+multiply(12, 4);
+
+function divide(a, b) {
+    return a / b;
+}
+divide(44, 11);
+
+function increment(n) {
+    n = n + 1;
+    return n;
+
+}
+increment(552);
+
+function decrement(n) {
+    n = n - 1;
+    return n;
+
+}
+decrement(798);
+
+function makeInt(n) {
+    parseInt(n);
+    return n;
+}
+makeInt("sldkjflksjf", 10);
+
+
+function preserveDecimal(n) {
+    parseFloat(n);
+    return n;
+}
+preserveDecimal("2.222");
